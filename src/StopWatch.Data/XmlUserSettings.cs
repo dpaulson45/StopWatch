1aed893ef87eb1c03d5b9425e47f24085f821e9e
David Paulson <dpaul@microsoft.com>
2019-07-01 23:11:00 -0500

    Added Xml Reader and Writer to the project
    Going to be used for reading user settings.

---
+++ src/StopWatch.Data/XmlUserSettings.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace StopWatch.Data
+{
+    public class XmlUserSettings
+    {
+        private static XmlSerializer xmlSerializer;
+        private string fileName; 
+
+        public XmlUserSettings(string SaveToLocation, object SaveObjectType)
+        {
+            fileName = SaveToLocation;
+            xmlSerializer = new XmlSerializer(SaveObjectType.GetType());
+        }
+
+        public void SaveToFile(object SaveObject)
+        {
+            using (StreamWriter sw = new StreamWriter(fileName))
+            {
+                xmlSerializer.Serialize(sw, SaveObject);
+            }
+        }
+
+        public object ReadFromFile()
+        {
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                return xmlSerializer.Deserialize(sr) as object; 
+            }
+        }
+    }
+}
